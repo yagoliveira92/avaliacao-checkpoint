33edbba26e0c30f304e4580b13c705f6b2f48b3b
Nicolas Santos Melo <nicolassantosmelo06@gmail.com>
2026-04-03 09:20:30 -0300

    adicionado carts em promoEspeciais

--- lib/promosEspeciais.dart
+++ lib/promosEspeciais.dart
@@ -82,3 +82,75 @@ class PromocoesPage extends StatelessWidget {
     );
   }
 }
+
+// PRODUTOCARD
+class ProdutoCard extends StatelessWidget {
+  final String nome, preco, imagem;
+  const ProdutoCard({
+    super.key,
+    required this.nome,
+    required this.preco,
+    required this.imagem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Column(
+          children: [
+            Expanded(
+              child: Image.asset(
+                imagem,
+                width: double.infinity,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) => const Icon(
+                  Icons.broken_image,
+                  size: 40,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    nome,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey,
+                      fontFamily: 'FontesCorpo', // FONTE DE CORPO
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    'R\$ $preco',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      fontFamily: 'Fontescorpo', // FONTE DE CORPO
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
