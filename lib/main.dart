6ae08a47a5269cc40923254a4e6030b5ae6d5a9b
Nicolas Santos Melo <nicolassantosmelo06@gmail.com>
2026-04-03 10:36:52 -0300

    feat: atualização sequencia pagina

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vendas_2/AreaVerde.dart';
+import 'package:flutter_vendas_2/categorias_page.dart';
+import 'package:flutter_vendas_2/entradaPage.dart';
+import 'package:flutter_vendas_2/promosEspeciais.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,7 +13,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: main.dart(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: const [
+          // TOPO DO SITE (Banner e Menu)
+          EntradaPage(),
+
+          // SEÇÃO DE CATEGORIAS
+          CategoriasPage(),
+
+          // SEÇÃO DE PROMOÇÕES
+          PromocoesPage(),
+
+          SizedBox(height: 30),
+
+          // RODAPÉ
+          AreaVerde(),
+        ],
+      ),
     );
   }
 }
