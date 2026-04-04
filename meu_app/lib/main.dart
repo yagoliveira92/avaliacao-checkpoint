c2d3d0ae6357c186bfd86976c6c5082a2a4517b3
Passolarguio <158389988+Passolarguio@users.noreply.github.com>
2026-04-04 19:54:51 -0300

    feat: altera a tabela de itens para cards

--- meu_app/lib/main.dart
+++ meu_app/lib/main.dart
@@ -7,16 +7,17 @@ void main() {
 class Product {
   final String name;
   final double price;
+  final String image;
 
-  Product(this.name, this.price);
+  Product(this.name, this.price, this.image);
 }
 
 class MyApp extends StatelessWidget {
   final List<Product> products = [
-    Product("Produto 1", 1),
-    Product("Produto 2", 2),
-    Product("Produto 3", 3),
-    Product("Produto 4", 4),
+    Product("Celular", 1500, "https://via.placeholder.com/150"),
+    Product("Notebook", 3500, "https://via.placeholder.com/150"),
+    Product("Fone", 200, "https://via.placeholder.com/150"),
+    Product("Mouse", 150, "https://via.placeholder.com/150"),
   ];
 
   @override
@@ -37,53 +38,132 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 90,
-        backgroundColor: Color.fromARGB(255, 149, 185, 238),
+        backgroundColor: Color.fromARGB(255, 171, 198, 238),
         centerTitle: true,
-        title: Image.asset('assets/logo.png', height: 90),
+        title: Image.asset('assets/logo.png', height: 80),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
-            child: ListView(
-              children: products.map((product) {
-                return Card(
-                  margin: EdgeInsets.all(10),
-                  child: ListTile(
-                    title: Text(product.name),
-                    subtitle: Text("R\$ ${product.price}"),
-                    trailing: ElevatedButton(
-                      onPressed: () {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text("${product.name} comprado!")),
-                        );
-                      },
-                      child: Text("Comprar"),
-                    ),
+            child: GridView.builder(
+              padding: EdgeInsets.all(10),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // 2 Cards p/ linha
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 0.75, // Altura dos Cards
+              ),
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 5,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      // IMAGENS
+                      Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(12),
+                          ),
+                          child: Image.network(
+                            product.image,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+
+                      // TEXTOS
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              product.name,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 4),
+                            Text("R\$ ${product.price}"),
+                          ],
+                        ),
+                      ),
+
+                      // BOTÕES
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 8,
+                          vertical: 4,
+                        ),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("${product.name} comprado!"),
+                              ),
+                            );
+                          },
+                          child: Text("Comprar"),
+                        ),
+                      ),
+                    ],
                   ),
                 );
-              }).toList(),
+              },
             ),
           ),
+
+          // SEÇÃO DE INSERIR EMAIL
           Padding(
             padding: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text("Cadastre E-mail pra receber promoções:"),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(hintText: "Digite seu email"),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("Email cadastrado!")),
-                    );
-                  },
-                  child: Text("Cadastrar"),
-                ),
-              ],
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 5)],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Cadastre E-mail pra receber promoções:",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      hintText: "Digite seu email",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Email cadastrado!")),
+                      );
+                    },
+                    child: Text("Cadastrar"),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
